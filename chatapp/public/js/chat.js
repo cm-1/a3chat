be7beb6657fef212e3c9698f89d99b22893af9ef
cm-1 <christopher.mossman@ucalgary.ca>
2019-03-06 02:04:17 -0700

    Well, the scrolling and bottom-alignment works...

--- chatapp/public/js/chat.js
+++ chatapp/public/js/chat.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 	console.log("yo yo, worldaka");
 	let socket = io();
+	let messages = $("#message-pane");
+	
+	messages.scrollTop(messages.prop('scrollHeight'));
 
 	$('form').submit(function(e){
 		e.preventDefault(); // prevents page reloading
-		socket.emit('chat message', $('#m').val());
-		$('#m').val('');
+		socket.emit('chat message', $('#msg-input').val());
+		$('#msg-input').val('');
+		messages.scrollTop(messages.prop('scrollHeight'));
 		return false;
 	});
 	
@@ -14,6 +18,15 @@ $(document).ready(function() {
 		I'm using a modification of this for now, to test bottom-alignment and scrolling.
 		CHANGE THE HTML! CHANGE THE CSS!
 		*/
-		$('#message-pane').append("<div class=\"message\">  <p>" + msg + "</p>  <span class=\"timestamp\">11:00</span></div>");
+		
+		//https://stackoverflow.com/questions/25505778/automatically-scroll-down-chat-div
+		
+		let shouldScroll = messages.prop('scrollTop') + messages.prop('clientHeight') === messages.prop('scrollHeight');
+		
+		$('#message-pane').append("<div class=\"message\">  <p>YOU: <span class=\"timestamp\">11:00</span><br><br>" + msg + "</p>  </div>");
+		
+		if(shouldScroll){
+			messages.scrollTop(messages.prop('scrollHeight'));
+		}
     });
 });
