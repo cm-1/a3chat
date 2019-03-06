71d3ef66fb0fe5225dc888da145e3432fe554dd7
cm-1 <christopher.mossman@ucalgary.ca>
2019-03-05 22:43:48 -0700

    Just finished tutorial.

---
+++ chatapp/public/js/chat.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	console.log("yo yo, worldaka");
+	let socket = io();
+
+	$('form').submit(function(e){
+		e.preventDefault(); // prevents page reloading
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+		return false;
+	});
+	
+	socket.on('chat message', function(msg){
+		$('#messages').append($('<li>').text(msg));
+    });
+});
